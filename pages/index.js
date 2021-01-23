ba648c5a59133a72e0fb468b7631d1a3d1406cd2
Lucas Constantino <lucas.c968@gmail.com>
2021-01-23 00:51:06 -0300

    Link test

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Link from 'next/link';
 
 function Home() {
     const [counter, setCounter] = useState(0);
@@ -8,6 +9,7 @@ function Home() {
             <h1>Home</h1>
             <p>Contador: {counter}</p>
             <button onClick={() => setCounter(counter => counter + 1)}>Adicionar</button>
+            <Link href="/sobre">Sobre</Link>
         </div>
     );
 }
